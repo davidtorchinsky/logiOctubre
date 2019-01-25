e383858169a505fb85464e47e4a3a5ac229c8ae9
Gabo <cancellieri.gabriel@gmail.com>
2019-01-25 17:44:38 -0300

    Editar Medico Modal

--- client/src/app/medico/medico.component.ts
+++ client/src/app/medico/medico.component.ts
@@ -12,7 +12,7 @@ import { NgForm } from '@angular/forms';
 })
 export class MedicoComponent implements OnInit {
   model: any = {};
-  medicos: Medico[];
+  medicos: Medico[] = [];
   cols: any[];
   selectedMedico: Medico;
   modalAgregarMedico = false;
@@ -22,14 +22,12 @@ export class MedicoComponent implements OnInit {
 
   constructor(
     private medicoService: MedicoService
-  ) {
-      this.medicos = [
-        {_id: '01', dni: '1234', nombre: 'John', apellido: 'Doe', telefono: '0800-1234-456', matricula: 'hola1'},
-        {_id: '02', dni: '12345', nombre: 'Anna', apellido: 'Smith',  telefono: '0800-1234-456', matricula: 'hola2'},
-        {_id: '03', dni: '123456', nombre: 'Peter', apellido: 'Jones',  telefono: '0800-1234-456', matricula: 'hola3'}
-      ];
-      //this.medicos =this.getTodosLosMedicos;
-  // this.getMedicos();
+  ) {}
+
+
+  ngOnInit() {
+      this.getMedicos();
+
       this.cols = [
         { field: 'dni', header: 'DNI' },
         { field: 'nombre', header: 'Nombre' },
@@ -39,16 +37,38 @@ export class MedicoComponent implements OnInit {
       ];
   }
 
-
-  ngOnInit() {
-    // this.getMedicos();
-  }
-
   getMedicos() {
-    this.medicoService.getMedicos()
+
+    const medico1 = new Medico();
+    medico1._id = '01';
+    medico1.dni = '1234';
+    medico1.nombre = 'John';
+    medico1.apellido = 'Doe';
+    medico1.telefono = '0800-1234-456';
+    medico1.matricula = 'hola1';
+
+    const medico2 = new Medico();
+    medico2._id = '02';
+    medico2.dni = '12345';
+    medico2.nombre = 'Anna';
+    medico2.apellido = 'Smith';
+    medico2.telefono = '0800-1234-456';
+    medico2.matricula = 'hola2';
+
+    const medico3 = new Medico();
+    medico3._id = '03';
+    medico3.dni = '123456';
+    medico3.nombre = 'Peter';
+    medico3.apellido = 'Jones';
+    medico3.telefono = '0800-1234-456';
+    medico3.matricula = 'hola3';
+
+    this.medicos.push(medico1, medico2, medico3);
+
+    /*this.medicoService.getMedicos()
     .then(medicos => {
         this.medicos = medicos;
-    });
+    });*/
   }
 
   cargarMedico(
@@ -64,17 +84,43 @@ export class MedicoComponent implements OnInit {
     f.resetForm();
   }
 
+  updateMedico(
+    dniMedico: string,
+    nombreMedico: string,
+    apellidoMedico: string,
+    telefonoMedico: string,
+    matriculaMedico: string,
+    f: NgForm
+  ) {
+    this.medicos.forEach(medico => {
+      if (medico._id === this.selectedMedico._id) {
+        medico.dni = this.selectedMedico.dni;
+        medico.nombre = this.selectedMedico.nombre;
+        medico.apellido = this.selectedMedico.apellido;
+        medico.matricula = this.selectedMedico.matricula;
+      }
+    });
+
+    this.selectedMedico = null;
+    f.resetForm();
+  }
+
   mostrarModalAgregarMedico() {
     this.modalAgregarMedico = true;
   }
 
  mostrarModalEditar() {
     if (this.selectedMedico != null) {
-      console.log('MEDICO: ' + this.selectedMedico);
+      console.log('MEDICO: ' + this.selectedMedico.dni);
        this.modalEditarMedico = true;
     }
 
   }
+
+  cerrarModalEditar() {
+    this.modalEditarMedico = false;
+  }
+
    mostrarModalEliminar(
      medico: Medico
    ) {
