c078ce5e6eec2f8898b84a1b268b2489f13606c3
davidtorchinsky <36799378+davidtorchinsky@users.noreply.github.com>
2019-02-04 18:13:41 -0300

    Add files via upload

---
+++ client/src/app/medicamento/medicamento.service.ts
@@ -0,0 +1,75 @@
+import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+import { UrlService } from '../shared/WindowProvider/window.provider.service';
+import { Medicamento } from './medicamento';
+import { default as swal } from 'sweetalert2';
+//import {medico2} from '../consultas/medicoConsultas.js';
+
+
+
+@Injectable()
+export class MedicamentoService {
+    private headers = new Headers({'Content-Type': 'application/json'});
+    private medicamentoURL = this.urlService.getRestApiUrl() + '/medicamento';  // URL a la api
+
+    constructor(
+        private http: Http,
+        private urlService: UrlService
+    ) {}
+
+    getMedicamentos(): Promise<Medicamento[]> {
+        console.log(this.medicamentoURL);
+        return this.http.get(this.medicamentoURL)
+        .toPromise()
+        .then(response => response.json().obj as Medicamento[])
+        .catch(this.handleError);
+    }
+
+    cargarMedicamento(
+        idMedMedicam: string,
+        nombreMedMedicam: string,
+        dosisMedMedicam: string,
+        cadenaDeFrioMedMedicam: string,
+        cantidadComprimidosMedicam: string): Promise<Medicamento> {
+
+        return this.http.post(this.medicamentoURL,
+            JSON.stringify({idMedicamento: idMedMedicam, nombreMedicamento: nombreMedMedicam,
+                dosisMedicamento: dosisMedMedicam, cadenaFrioMedicamento: cadenaDeFrioMedMedicam,
+                cantidadComprimidosMedicamento: cantidadComprimidosMedicam}), {headers: this.headers})
+        .toPromise()
+        .then(response => response.json().obj as Medicamento)
+        .catch(this.handleError);
+    }
+
+    editarMedicamento(
+        idMedMedicam: string,
+        nombreMedMedicam: string,
+        dosisMedMedicam: string,
+        cadenaDeFrioMedMedicam: string,
+        cantidadComprimidosMedicam: string): Promise<Medicamento> {
+        return this.http.patch(this.medicamentoURL + '/' + idMedMedicam,
+            JSON.stringify({nombreMedicamento: nombreMedMedicam,
+                dosisMedicamento: dosisMedMedicam, cadenaFrioMedicamento: cadenaDeFrioMedMedicam,
+                cantidadComprimidosMedicamento: cantidadComprimidosMedicam}), {headers: this.headers})
+        .toPromise()
+        .then(response => response.json().obj as Medicamento)
+        .catch(this.handleError);
+    }
+
+    deleteMedicamento(idMedMedicam: string): Promise<Medicamento> {
+        return this.http.delete(this.medicamentoURL + '/' + idMedMedicam)
+        .toPromise()
+        .then(response => response.json().obj as Medicamento)
+        .catch(this.handleError);
+    }
+
+    private handleError(error: any): Promise<any> {
+        console.error('Ocurrio un error en Servicio de Medicamentos: ', error);
+            swal(
+                'Error!',
+                error.json().error,
+                'error'
+            );
+        return Promise.reject(error.message || error);
+    }
+}
---
+++ client/src/app/medicamento/medicamento.ts
@@ -0,0 +1,8 @@
+export class Medicamento {
+    _id: string;
+    idMedicamento: string;
+    nombre: string;
+    dosis: string;
+    cadenaFrio: string;
+    cantidadComprimidos: string;
+}
